0fe2c6d555c64e9006037182e149b177747626f9
sytchov1 <sytchov1@gmail.com>
2021-12-22 01:00:20 +0300

    add comments

--- client.py
+++ client.py
@@ -1,17 +1,21 @@
-import asyncio
-import uuid
+# Импорт модулей
+import asyncio      # Для создания асинхронного сервера
+import uuid         # Для генерации уникального кода
 
 
-HOST = '127.0.0.1'
-PORT_1 = 8000
-PORT_2 = 8001
-BUFFER_SIZE = 64
+# Константы
+HOST = '127.0.0.1'  # Localhost
+PORT_1 = 8000       # Порт для уникальных кодов
+PORT_2 = 8001       # Порт для сообщений
+BUFFER_SIZE = 64    # Объём данных, считываемых с порта за раз
 
 
+# Функция, генерирующая уникальный идентификатор на основе MAC-адреса и текущего времени
 async def generate_uid():
     return str(uuid.uuid1())
 
 
+# Функция, устанавливающая подключение к порту
 async def connect(host, port):
     print('Подключаемся к серверу... (Убедитесь, что сервер включен)')
     while True:
@@ -23,6 +27,7 @@ async def connect(host, port):
     return r, w
 
 
+# Функция для отправки данных на сервер
 async def send_data(writer, data):
     print('Отправляем запрос на сервер...')
     try:
@@ -31,55 +36,59 @@ async def send_data(writer, data):
         print("Соединение с сервером потеряно")
 
 
+# Функция для считывания данных от сервера
 async def receive_data(reader):
     print("Ждём ответ...")
     d = bytearray()
     while True:
-        chunk = await reader.read(BUFFER_SIZE)
+        chunk = await reader.read(BUFFER_SIZE)  # Считываем часть данных
         d += chunk
         reader.feed_eof()
-        if reader.at_eof():
+        if reader.at_eof():                     # Если данные еще остались, то считываем повторно
             break
     return d.decode()
 
 
+# Функция, реализующая отправку идентификатора на сервер и получение кода
 async def get_code():
-    reader, writer = await connect(HOST, PORT_1)
+    reader, writer = await connect(HOST, PORT_1)                        # Подключаемся к порту 8000
     try:
-        while True:
+        while True:                                                     # Пока не получим код...
             print("Генерируем уникальный идентификатор...")
-            uid = await generate_uid()
+            uid = await generate_uid()                                  # Генерируем идентификатор
 
-            await send_data(writer, uid)
+            await send_data(writer, uid)                                # Отправляем данные на сервер
 
-            response = await receive_data(reader)
+            response = await receive_data(reader)                       # Принимаем ответ от сервера
 
-            if response:
-                print(f"Ваш уникальный идентификатор (UID): {uid}")
+            if response:                                                # Если код получен,
+                print(f"Ваш уникальный идентификатор (UID): {uid}")     # то выводим код и идентификатор
                 print(f'Ваш код (CODE): {response}')
                 break
     finally:
-        writer.close()
+        writer.close()                                                  # Отключаемся от сервера
         await writer.wait_closed()
         print("Соединение закрыто\n")
 
 
+# Функция, реализующая отправку сообщения на сервер и получение ответа
 async def send_message(params):
-    reader, writer = await connect(HOST, PORT_2)
+    reader, writer = await connect(HOST, PORT_2)                        # Подключаемся к порту 8001
     try:
-        await send_data(writer, params)
+        await send_data(writer, params)                                 # Отправляем данные на сервер
 
-        response = await receive_data(reader)
+        response = await receive_data(reader)                           # Получаем ответ от сервера
         print(f'<-- {response}')
     finally:
-        writer.close()
+        writer.close()                                                  # Отключаемся от сервера
         await writer.wait_closed()
         print("Соединение закрыто\n")
 
 
+# Основной цикл клиентского приложения
 def main():
     try:
-        asyncio.run(get_code())
+        asyncio.run(get_code())                     # Получаем код от сервера
 
         print('Для отправки сообщения введите его текст, ваш уникальный идентификатор и код, полученный от сервера, '
               'в формате: "Текст сообщения" UID CODE')
@@ -91,7 +100,7 @@ def main():
             elif temp == "exit":
                 break
             else:
-                asyncio.run(send_message(temp))
+                asyncio.run(send_message(temp))     # Отправляем сообщение на сервер
     except KeyboardInterrupt:
         pass
 
--- server.py
+++ server.py
@@ -1,24 +1,28 @@
-import asyncio
-import uuid
-import logging
+# Импорт модулей
+import asyncio      # Для создания асинхронного сервера
+import uuid         # Для генерации уникального кода
+import logging      # Для ведения лога сообщений
 
+# Константы
+HOST = '127.0.0.1'  # Localhost
+PORT_1 = 8000       # Порт для уникальных кодов
+PORT_2 = 8001       # Порт для сообщений
+BUFFER_SIZE = 64    # Объём данных, считываемых с порта за раз
 
-HOST = '127.0.0.1'
-PORT_1 = 8000
-PORT_2 = 8001
-BUFFER_SIZE = 64
-
-CLIENTS = {}
 logging.basicConfig(filename='server.log',
                     format='%(asctime)s | %(message)s',
                     datefmt='%d-%m-%y %H:%M:%S',
-                    level=logging.INFO)
+                    level=logging.INFO)     # Настройки лог-файла
+
+clients = {}        # Информация о клиентах за текущую сессию
 
 
+# Функция для генерации уникального кода на основе случайных значений
 async def generate_code():
     return str(uuid.uuid4())
 
 
+# Функция для распаковки параметров из принятой строки
 async def extract_params(data):
     try:
         message, temp = data[1:].split('\" ')
@@ -29,6 +33,7 @@ async def extract_params(data):
     return result
 
 
+# Функция для установки порта в режим прослушивания
 async def open_port(host, port, handler):
     s = None
     try:
@@ -40,71 +45,75 @@ async def open_port(host, port, handler):
     return s
 
 
+# Функция для считывания данных от клиента
 async def receive_data(reader):
     d = bytearray()
     while True:
-        chunk = await reader.read(BUFFER_SIZE)
+        chunk = await reader.read(BUFFER_SIZE)  # Считываем часть данных
         d += chunk
         reader.feed_eof()
-        if reader.at_eof():
+        if reader.at_eof():                     # Если данные еще остались, то считываем повторно
             break
     return d.decode()
 
 
+# Функция для отправки данных клиенту
 async def send_data(writer, data):
     try:
         writer.write(data.encode())
-        await writer.drain()
+        await writer.drain()    # Очищаем буффер
         print(f"Ответ для {writer.get_extra_info('peername')} отправлен")
     except ConnectionError:
         print("Соединение с клиентом потеряно")
 
 
+# Функция-обработчик, принимающая уникальный идентификатор и возвращающая уникальный код
 async def handle_code(reader, writer):
     client = writer.get_extra_info('peername')
     print(f"Клиент {client} присоединился к порту {PORT_1}")
 
     while True:
-        flag_ok = False
+        flag_ok = False     # Флаг для проверки уникальности идентификатора
 
-        data = await receive_data(reader)
+        data = await receive_data(reader)       # Считываем идентификатор клиента
         print(f"Получены данные от  {client}")
 
         if data:
-            if data not in CLIENTS:
-                CLIENTS[data] = await generate_code()
+            if data not in clients:                     # Если клиента с таким uid еще не было в текущей сессии,
+                clients[data] = await generate_code()   # то генерируем код и сохраняем для текущей сессии
 
-                await send_data(writer, CLIENTS[data])
+                await send_data(writer, clients[data])  # Отправляем код клиенту
                 flag_ok = True
             else:
-                await send_data(writer, "")
+                await send_data(writer, "")   # Если полученный id не уникальный, то просим клиента прислать новый
         else:
             await send_data(writer, "Отправлены некорректные данные")
 
         if flag_ok:
             break
 
-    writer.close()
+    writer.close()      # Закрываем соединение с данным клиентом
     await writer.wait_closed()
     print(f"{client} отсоединён от порта {PORT_1}")
 
 
+# Функция-обработчик, принимающая сообщение и возвращающая ответ клиенту
 async def handle_message(reader, writer):
     client = writer.get_extra_info('peername')
     print(f"Клиент {client} присоединился к порту {PORT_2}")
 
-    data = await receive_data(reader)
+    data = await receive_data(reader)       # Считываем данные от клиента
     print(f"Получены данные от  {client}")
 
-    params = await extract_params(data)
+    params = await extract_params(data)     # Распаковываем данные
     if params:
         message, uid, code = params
 
-        if uid in CLIENTS:
-            if code == CLIENTS[uid]:
-                logging.info("%s", message)
+        if uid in clients:                                              # Если информация о клиенте имеется
+            if code == clients[uid]:                                    # и получен верный код,
+                logging.info("%s", message)                             # то записываем сообщение в лог-файл
                 print(f"Сообщение от {client} добавлено в лог")
-                await send_data(writer, "Сообщение добавлено в лог")
+                await send_data(writer, "Сообщение добавлено в лог")    # и отправляем ответ клиенту
             else:
                 await send_data(writer, "Неверный код (CODE)")
         else:
@@ -112,36 +121,37 @@ async def handle_message(reader, writer):
     else:
         await send_data(writer, 'Введённые данные не соответствуют требуемому формату: "Текст сообщения" UID CODE')
 
-    writer.close()
+    writer.close()      # Закрываем соединение с данным клиентом
     await writer.wait_closed()
     print(f"{client} отсоединён от порта {PORT_2}")
 
 
+# Функция, реализующая main_loop
 async def start():
-    code_serv = await open_port(HOST, PORT_1, handle_code)
-    message_serv = await open_port(HOST, PORT_2, handle_message)
+    code_serv = await open_port(HOST, PORT_1, handle_code)          # Открываем порт 8000
+    message_serv = await open_port(HOST, PORT_2, handle_message)    # Открываем порт 8001
 
     try:
-        if code_serv and message_serv:
+        if code_serv and message_serv:                              # Если порты открыты успешно
             async with code_serv, message_serv:
-                await code_serv.serve_forever()
-                await message_serv.serve_forever()
+                await code_serv.serve_forever()                     # Бесконечно прослушиваем 8000
+                await message_serv.serve_forever()                  # Бесконечно прослушиваем 8001
         else:
             print("Не удалось открыть один или более портов. Работа сервера будет прекращена")
             input("***Нажмите Enter***")
     finally:
         if code_serv:
-            code_serv.close()
+            code_serv.close()                                       # Закрываем порт 8000
             await code_serv.wait_closed()
         if message_serv:
-            message_serv.close()
+            message_serv.close()                                    # Закрываем порт 8001
             await message_serv.wait_closed()
 
 
 def main():
     print("Для завершения работы нажмите Ctrl+C\n")
     try:
-        asyncio.run(start())
+        asyncio.run(start())    # Запускаем основной цикл событий
     except KeyboardInterrupt:
         pass
 
